d51599feda4ac016e16722f2c4242beb3f8d4930
CherubRocky <zar@ciencias.unam.mx>
2024-09-13 18:25:26 -0600

    Se completó la clase Usuario y se creó el enum Status

--- src/Modelo/Usuario.cpp
+++ src/Modelo/Usuario.cpp
@@ -1,8 +1,40 @@
 #include <string>
+#include "Status.cpp"
 using namespace std;
 class Usuario {
 public:
-    string getApodo() {return apodo;}
+    Usuario(string nickname, int socket, Status stat) {
+        apodo = nickname;
+        socketfd = socket;
+        status = stat;
+    }
+
+    string getApodo() {
+        return apodo;
+    }
+
+    int getSocketFd() {
+        return socketfd;
+    }
+
+    string getStatus() {
+        switch (status) {
+            case Status::ACTIVE:
+                return "ACTIVE";
+            case Status::AWAY:
+                return "AWAY";
+            case Status::BUSY:
+                return "BUSY";
+        }
+    }
+
+    void setStatus(Status stat) {
+        status = stat;
+    }
+    
 private:
     string apodo;
+    int socketfd;
+    Status status;
+
 };
